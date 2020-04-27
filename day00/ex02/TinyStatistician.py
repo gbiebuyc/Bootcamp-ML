b44d48a4dbcd87289f234dabd1f416ee19a90c83
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-04-27 21:55:35 +0200

    stuff

--- day00/ex02/TinyStatistician.py
+++ day00/ex02/TinyStatistician.py
@@ -5,8 +5,11 @@ class TinyStatistician:
         return sum(x) / len(x)
 
     def median(self, x):
+        x.sort()
         n = len(x)
         if n % 2:
             return x[n//2]
         return (x[n/2-1]+x[n/2])/2
 
+    def quartiles(self, x, percentile):
+        return sorted(x)[int(len(x)*percentile*0.01)]
--- day00/ex02/test.py
+++ day00/ex02/test.py
@@ -6,3 +6,5 @@ a = [1, 42, 300, 10, 59]
 print('array:', a)
 print('mean:', tstat.mean(a))
 print('median:', tstat.median(a))
+print('quartile 25:', tstat.quartiles(a, 25))
+print('quartile 75:', tstat.quartiles(a, 75))
