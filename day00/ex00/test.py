74fe7c1b982eeec24486fbcec5ef36daad72caf8
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-04-27 18:19:23 +0200

    stuff

--- day00/ex00/test.py
+++ day00/ex00/test.py
@@ -2,5 +2,32 @@
 from vector import Vector
 
 v1 = Vector([0.0, 1.0, 2.0, 3.0])
+assert v1.values == [0.0, 1.0, 2.0, 3.0]
+assert v1.size == 4
+
+# add
+v2 = v1 + 5
+assert v1.values == [0.0, 1.0, 2.0, 3.0]
+assert v2.values == [5.0, 6.0, 7.0, 8.0]
+v2 = 5 + v1
+assert v2.values == [5.0, 6.0, 7.0, 8.0]
+v2 = v1 + v2
+assert v2.values == [5.0, 7.0, 9.0, 11.0]
+
+# sub
+v2 = v1 - 5
+assert v2.values == [-5.0, -4.0, -3.0, -2.0]
+v2 = 5 - v1
+assert v2.values == [-5.0, -4.0, -3.0, -2.0]
+v2 = v1 - v2
+assert v2.values == [5.0, 5.0, 5.0, 5.0]
+
+# mul
 v2 = v1 * 5
-print(v2)
+assert v2.values == [0.0, 5.0, 10.0, 15.0]
+v2 = 5 * v1
+assert v2.values == [0.0, 5.0, 10.0, 15.0]
+dot_prod = v1 * v2
+assert dot_prod == 70
+
+print('All good.')
--- day00/ex00/vector.py
+++ day00/ex00/vector.py
@@ -1,5 +1,41 @@
-import numpy as np
-
 class Vector:
     def __init__(self, arg):
-        pass
+        self.values = arg
+        self.size = len(arg)
+
+    def __add__(self, other):
+        if type(other) in (int, float):
+            return Vector([x+other for x in self.values])
+        if type(other) is Vector:
+            if other.size != self.size:
+                raise ValueError("Vector sizes don't match")
+            return Vector([x + y for x, y in zip(self.values, other.values)])
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + other * -1
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __mul__(self, other):
+        if type(other) in (int, float):
+            return Vector([x*other for x in self.values])
+        if type(other) is Vector:
+            if other.size != self.size:
+                raise ValueError("Vector sizes don't match")
+            dot_prod = 0
+            for x, y in zip(self.values, other.values):
+                dot_prod += x * y
+            return dot_prod
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __str__(self):
+        return '(Vector %s)' % str(self.values)
+
+    def __repr__(self):
+        return '(Vector %s)' % str(self.values)
