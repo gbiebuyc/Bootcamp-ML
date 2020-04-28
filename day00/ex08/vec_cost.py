0f9a7fa012541541a3cce8a05d48b3e8dcf31127
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-04-28 23:05:17 +0200

    stuff

--- day00/ex08/vec_cost.py
+++ day00/ex08/vec_cost.py
@@ -1,18 +1,4 @@
 import numpy as np
 
 def cost_(y, y_hat):
-    """Computes the mean squared error of two non-empty numpy.ndarray, without any for loop.
-    ,→ The two arrays must have the same dimensions.
-    Args:
-    y: has to be an numpy.ndarray, a vector.
-    y_hat: has to be an numpy.ndarray, a vector.
-    Returns:
-    The mean squared error of the two vectors as a float.
-    None if y or y_hat are empty numpy.ndarray.
-    None if y and y_hat does not share the same dimensions.
-    Raises:
-    This function should not raise any Exceptions.
-    """
-
-    # I adapted the code from there: https://towardsdatascience.com/vectorization-implementation-in-machine-learning-ca652920c55d
-    return (1/(2*len(y)))*np.transpose((y_hat - y))@(y_hat - y)
+    return (1/(2*len(y)))*np.sum((y - y_hat)**2)
